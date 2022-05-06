f75c33b04f9fb1cf32ca2e34ae24c77a4331e0b7
tayar3 <m.altayar@nu.edu.eg>
2022-05-06 14:50:31 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+
+from flask import Flask, jsonify,request
+from flask_restful import Resource, Api
+import pickle
+import pandas as pd
+from flask_cors import CORS
+from model import *
+from utils import *
+import numpy as np
+import xgboost as xgb
+
+
+
+
+app = Flask(__name__)
+#
+CORS(app)
+# creating an API object
+api = Api(app)
+
+#prediction api call
+class prediction(Resource):
+    def post(self):
+        cust=request.get_json('Data')
+        #cust=np.array(cust)
+        x_vars_list = readData(cust)
+
+
+        print(cust)
+        
+        model = pickle.load(open('model.pkl', 'rb'))
+        test_X = np.array(x_vars_list)
+        print(test_X)
+        xgtest = xgb.DMatrix(test_X)
+        preds = model.predict(xgtest)
+        print (preds)
+
+        target_cols = ['Saving Account','Guarantees','Current Account','Derivada Account','Payroll Account','Junior Account','Más particular Account','particular Account','particular Plus Account','Short-term Deposits','Medium-term Deposits','Long-term Deposits','e-account','Funds','Mortgage','Pensions','Loans','Taxes','Credit Card','Securities','Home Account','Payroll','Pensions','Direct Debit']
+        target_cols = target_cols[2:]
+        target_cols = np.array(target_cols)
+        preds = np.argsort(preds, axis=1)
+        print (preds)
+        preds = np.fliplr(preds)[:,:7]
+        
+
+
+        print (preds)
+        
+        final_preds = [", ".join(list(target_cols[pred])) for pred in preds]
+        out_df = jsonify(final_preds)
+        
+        
+        
+        #prediction = int(prediction[0])
+        return out_df
+
+
+
+api.add_resource(prediction, '/prediction/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
